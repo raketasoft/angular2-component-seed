0eec9b966ef5afc3f55415b7ba220fefb77a78f8
Branimir Borisov <branimir@raketasoft.com>
2016-04-14 23:58:59 +0300

    style: fix project indentation

--- examples/basic-example.component.ts
+++ examples/basic-example.component.ts
@@ -2,11 +2,11 @@ import {Component} from 'angular2/core';
 import {NgComponent} from '../ng.component';
 
 @Component({
-    selector: 'basic-example',
-    template: `
+  selector: 'basic-example',
+  template: `
 <h2>Basic Example</h2>
 <ng-component>Loading...</ng-component>
 `,
-    directives: [NgComponent]
+  directives: [NgComponent]
 })
 export class BasicExampleComponent { }
--- src/ng.component.ts
+++ src/ng.component.ts
@@ -1,7 +1,7 @@
 import {Component} from 'angular2/core';
 
 @Component({
-    selector: 'ng-component',
-    template: '<p>My Angular 2 Component</p>'
+  selector: 'ng-component',
+  template: '<p>My Angular 2 Component</p>'
 })
 export class NgComponent { }
