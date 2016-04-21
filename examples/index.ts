b61797771b2a72bae4881f4381620aaf97ca616f
Branimir Borisov <branimir@raketasoft.com>
2016-04-21 22:49:18 +0300

    style: clean spaces

--- examples/index.ts
+++ examples/index.ts
@@ -1,4 +1,4 @@
-import {bootstrap}    from 'angular2/platform/browser';
+import {bootstrap} from 'angular2/platform/browser';
 import {BasicExampleComponent} from './basic-example.component';
 
 bootstrap(BasicExampleComponent);
