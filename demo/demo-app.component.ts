8e42ea02100b901c3bf2ea9453ed9242a5b424eb
Branimir Borisov <branimir@raketasoft.com>
2016-04-29 11:34:32 +0300

    style(component): fix html template indent

--- demo/demo-app.component.ts
+++ demo/demo-app.component.ts
@@ -4,9 +4,8 @@ import {NgComponent} from '../ng-component';
 @Component({
   selector: 'demo-app',
   template: `
-<h2>Basic Example</h2>
-<ng-component>Loading...</ng-component>
-`,
+    <h2>Basic Example</h2>
+    <ng-component>Loading...</ng-component>`,
   directives: [NgComponent]
 })
 export class DemoAppComponent { }
